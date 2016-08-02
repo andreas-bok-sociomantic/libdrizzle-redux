1eb3c0ea350040252bffd66f693da9434548ba16
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2016-08-02 18:42:32 +0200

    Support colored logging to terminal
    Adds
    - ANSI color codes as constants
    - macros for colored msgs when entering and exiting
       funcs

--- libdrizzle-5.1/constants.h
+++ libdrizzle-5.1/constants.h
@@ -51,6 +51,19 @@
 extern "C" {
 #endif
 
+#define RED   "\x1B[31m"
+#define REDB  "\x1B[1;31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+#define _FN_IN_  GRN "ENTER" RESET
+#define _FN_OUT_ RED "EXIT " RESET
+
 /**
  * @addtogroup drizzle_constants Constants
  * @ingroup drizzle_client_interface
