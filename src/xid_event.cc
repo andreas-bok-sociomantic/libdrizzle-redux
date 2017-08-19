ca354c0950270e3b9a4185faedc72cd8dc6a8a46
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-08-19 16:04:46 +0200

    Add XID_EVENT API and implementation

---
+++ src/xid_event.cc
@@ -0,0 +1,24 @@
+#include "config.h"
+#include "src/common.h"
+
+drizzle_binlog_event_st* drizzle_binlog_xid_event_header(
+    drizzle_binlog_xid_event_st* event)
+{
+    return &event->header;
+}
+
+uint64_t drizzle_binlog_xid_event_xid(drizzle_binlog_xid_event_st* event)
+{
+    return event->xid;
+}
+
+
+drizzle_binlog_xid_event_st *drizzle_binlog_get_xid_event(
+    drizzle_binlog_event_st *event)
+{
+    auto *xid_event = new drizzle_binlog_xid_event_st();
+    set_event_header(&xid_event->header, event);
+    xid_event->xid = (uint64_t) drizzle_get_byte8(event->data_ptr);
+    event->data_ptr += 8;
+    return xid_event;
+}
