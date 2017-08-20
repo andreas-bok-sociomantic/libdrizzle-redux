4df92cdc7f02b7bf05736f896402de05b1dba3d6
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-08-20 20:50:52 +0200

    Add TABLEMAP_EVENT implementation

---
+++ include/libdrizzle-redux/tablemap_event.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+ * @brief      Get a table map event
+ *
+ * @param      event  a drizzle_binlog_event_st
+ *
+ * @return     a drizzle_binlog_tablemap_event_st
+ */
+DRIZZLE_API
+drizzle_binlog_tablemap_event_st *drizzle_binlog_get_tablemap_event(
+    drizzle_binlog_event_st *event);
+
+
+/**
+ * @brief      Unique id assigned to a table by the MySQL server
+ *
+ * @return     a table id
+ */
+DRIZZLE_API
+uint64_t drizzle_binlog_tablemap_event_table_id(
+	drizzle_binlog_tablemap_event_st* event);
+
+/**
+ * @brief      A database schema name
+ *
+ * @return     database name
+ */
+DRIZZLE_API
+unsigned char* drizzle_binlog_tablemap_event_schema_name(
+	drizzle_binlog_tablemap_event_st* event);
+
+/**
+ * @brief      A database table name
+ *
+ * @return     table name
+ */
+DRIZZLE_API
+unsigned char* drizzle_binlog_tablemap_event_table_name(
+	drizzle_binlog_tablemap_event_st* event);
+
+/**
+ * @brief      The number of columns in the table
+ *
+ * @return     number of columns
+ */
+DRIZZLE_API
+uint64_t drizzle_binlog_tablemap_event_column_count(
+	drizzle_binlog_tablemap_event_st* event);
+
+/**
+ * @brief      An array of column types, one byte per column.
+ *             To find the meanings of these values, look at
+ *             enum_field_types in the mysql_com.h header file.
+ *
+ * @return     an array of column types
+ */
+DRIZZLE_API
+uint8_t* drizzle_binlog_tablemap_event_column_type_def(
+	drizzle_binlog_tablemap_event_st* event);
+
+/**
+ * @brief      Metadata for table columns
+ *
+ *             \see log_event.h in the MySQL source code for contents and
+ *             format.
+ *
+ * @return     array of metadata
+ */
+DRIZZLE_API
+uint8_t* drizzle_binlog_tablemap_event_field_metadata(
+	drizzle_binlog_tablemap_event_st* event);
+
+/**
+ * @brief      Length of the field_metadata array.
+ *
+ * @return     Size of the field_metadata array in bytes
+ */
+DRIZZLE_API
+uint64_t drizzle_binlog_tablemap_event_field_metadata_len(
+	drizzle_binlog_tablemap_event_st* event);
+
+/**
+ * @brief      Bit-field indicating whether each column can be NULL, one bit
+ *             per column
+ *
+ * @return     A bitmap
+ */
+DRIZZLE_API
+uint8_t* drizzle_binlog_tablemap_event_null_bitmap(
+	drizzle_binlog_tablemap_event_st* event);
+
+#ifdef __cplusplus
+}
+#endif
+
---
+++ src/tablemap_event.cc
@@ -0,0 +1,102 @@
+#include "config.h"
+#include "src/common.h"
+
+drizzle_binlog_tablemap_event_st *drizzle_binlog_get_tablemap_event(
+    drizzle_binlog_event_st *event)
+{
+
+    auto tablemap_event = new drizzle_binlog_tablemap_event_st();
+    set_event_header(&tablemap_event->header, event);
+
+    tablemap_event->table_id = drizzle_get_byte6(event->data_ptr);
+    event->data_ptr+=6;
+
+    memcpy(&tablemap_event->flags, event->data_ptr, 2);
+    event->data_ptr+=2;
+
+    uint8_t len_enc = event->data_ptr[0];
+    memcpy(&tablemap_event->schema_name, event->data_ptr+1, len_enc);
+    tablemap_event->schema_name[len_enc] = '\0';
+    event->data_ptr+=len_enc + 1;
+
+    event->data_ptr++;
+
+    len_enc = event->data_ptr[0];
+    memcpy(&tablemap_event->table_name, event->data_ptr+1, len_enc);
+    tablemap_event->table_name[len_enc] = '\0';
+    event->data_ptr+=len_enc + 1;
+
+    event->data_ptr++;
+
+    tablemap_event->column_count = drizzle_binlog_get_encoded_len(event);
+
+    tablemap_event->column_type_def =
+        (uint8_t*) malloc(tablemap_event->column_count);
+    memcpy(&tablemap_event->column_type_def, event->data_ptr,
+        tablemap_event->column_count);
+    event->data_ptr+=tablemap_event->column_count;
+
+    len_enc = drizzle_binlog_get_encoded_len(event);
+    tablemap_event->field_metadata_len = len_enc + 1;
+    tablemap_event->field_metadata =
+        (uint8_t*) malloc(tablemap_event->field_metadata_len);
+    memcpy(&tablemap_event->field_metadata, event->data_ptr, len_enc);
+    event->data_ptr+=len_enc;
+
+    len_enc = (uint32_t) (tablemap_event->column_count + 7) / 8;
+    tablemap_event->null_bitmap = (uint8_t*)malloc(len_enc);
+    memcpy(&tablemap_event->null_bitmap, event->data_ptr, len_enc);
+    event->data_ptr+=len_enc;
+
+    return tablemap_event;
+}
+
+
+
+uint64_t drizzle_binlog_tablemap_event_table_id(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->table_id;
+}
+
+unsigned char *drizzle_binlog_tablemap_event_schema_name(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->schema_name;
+}
+
+unsigned char *drizzle_binlog_tablemap_event_table_name(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->table_name;
+}
+
+uint64_t drizzle_binlog_tablemap_event_column_count(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->column_count;
+}
+
+uint8_t *drizzle_binlog_tablemap_event_column_type_def(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->column_type_def;
+}
+
+uint8_t *drizzle_binlog_tablemap_event_field_metadata(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->field_metadata;
+}
+
+uint64_t drizzle_binlog_tablemap_event_field_metadata_len(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->field_metadata_len;
+}
+
+uint8_t *drizzle_binlog_tablemap_event_null_bitmap(
+	drizzle_binlog_tablemap_event_st* event)
+{
+    return event->null_bitmap;
+}
