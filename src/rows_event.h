c084a7a9905799856598f9bc562c3a188187b354
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-08-21 00:40:33 +0200

    RowsEvent: add table_name member

--- src/rows_event.h
+++ src/rows_event.h
@@ -5,6 +5,7 @@ struct drizzle_binlog_rows_event_st
     drizzle_binlog_event_st header;
 
     uint64_t table_id;
+    char table_name[DRIZZLE_MAX_TABLE_SIZE];
     uint64_t column_count;
     uint32_t field_metadata_len;
     drizzle_column_type_t *column_type_def;
