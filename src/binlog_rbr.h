dcd6f5dc374b87512fa38792aa0795d9b659b370
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-08-22 23:55:14 +0200

    Add map

--- src/binlog_rbr.h
+++ src/binlog_rbr.h
@@ -1,22 +1,26 @@
 #pragma once
 
-#include <map>
+#include <unordered_map>
 
 struct drizzle_binlog_rbr_st
 {
     drizzle_binlog_xid_event_st xid_event;
     drizzle_binlog_query_event_st query_event;
-    drizzle_binlog_tablemap_event_st *tablemap_events;
+    typedef std::unordered_map<uint64_t, drizzle_binlog_tablemap_event_st*>
+        map_tablemap_events;
+    map_tablemap_events tablemap_events;
     drizzle_binlog_rows_event_st *rows_events;
     drizzle_binlog_st *binlog;
 
     uint64_t xid;
 
     drizzle_binlog_rbr_st() :
-    tablemap_events(NULL),
     rows_events(NULL),
     binlog(NULL)
     {
+    }
 
+    ~drizzle_binlog_rbr_st()
+    {
     }
 };
