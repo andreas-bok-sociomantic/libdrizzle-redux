a43f82f2f6860589f7be61f25dd0fe7409b3b38b
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-08-26 14:41:57 +0200

    Add RowsEvent to rbr implementation

--- src/binlog_rbr.cc
+++ src/binlog_rbr.cc
@@ -5,3 +5,19 @@ uint64_t drizzle_binlog_rbr_xid(drizzle_binlog_rbr_st *binlog_rbr)
 {
     return binlog_rbr->xid_event.xid;
 }
+
+drizzle_binlog_tablemap_event_st * drizzle_binlog_rbr_st::get_tablemap_event(uint64_t table_id)
+{
+    if (tablemap_events.find(table_id) == tablemap_events.end())
+    {
+        tablemap_events.insert(std::make_pair(table_id, new drizzle_binlog_tablemap_event_st()));
+    }
+
+    return tablemap_events.find(table_id)->second;
+}
+
+drizzle_binlog_rows_event_st * drizzle_binlog_rbr_st::get_rows_event()
+{
+    rows_events.push_back(new drizzle_binlog_rows_event_st());
+    return rows_events.back();
+}
