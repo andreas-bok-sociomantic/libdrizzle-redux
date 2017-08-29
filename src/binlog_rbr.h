e2ae4d4a8c77ee801e892046674ca92f04f8e82f
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-08-29 22:44:44 +0200

    More RBR implementation

--- src/binlog_rbr.h
+++ src/binlog_rbr.h
@@ -2,21 +2,25 @@
 
 #include <unordered_map>
 #include <vector>
+#include <iterator>
 
 struct drizzle_binlog_rbr_st
 {
+    drizzle_binlog_st *binlog;
+    drizzle_binlog_rbr_fn *binlog_rbr_fn;
     drizzle_binlog_xid_event_st xid_event;
     drizzle_binlog_query_event_st query_event;
     typedef std::unordered_map<uint64_t, drizzle_binlog_tablemap_event_st*>
         map_tablemap_events;
     map_tablemap_events tablemap_events;
-    std::vector<drizzle_binlog_rows_event_st*> rows_events;
-    drizzle_binlog_st *binlog;
-
-    uint64_t xid;
+    typedef std::vector<drizzle_binlog_rows_event_st*> vec_row_events;
+    vec_row_events rows_events;
+    size_t rows_count;
 
     drizzle_binlog_rbr_st() :
-    binlog(NULL)
+        binlog(NULL),
+        binlog_rbr_fn(NULL),
+        rows_count(0)
     {
     }
 
@@ -25,6 +29,31 @@ struct drizzle_binlog_rbr_st
 
     }
 
+    void reset();
+
+    /**
+     * Gets a tablemap event.
+     *
+     * @param[in]  table_id  The table identifier
+     *
+     * @return     Pointer to a tablemap event struct
+     */
     drizzle_binlog_tablemap_event_st * get_tablemap_event(uint64_t table_id);
+
+    /**
+     * Get a rows event
+     * If the current number of rows exceeds the size of the vector a new
+     * row event struct is created.
+     * Else the next unused row struct is returned
+     *
+     * @return     Pointer a rows event struct
+     */
     drizzle_binlog_rows_event_st * get_rows_event();
+
+    /**
+     * Adds a drizzle binlog event
+     *
+     * @param      binlog_event  a drizzle binlog event struct
+     */
+    void add_binlog_event(drizzle_binlog_event_st* binlog_event);
 };
