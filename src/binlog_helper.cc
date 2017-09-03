d0f8607756bc745fadf65a46ccdc9a396dcc6be6
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-09-03 20:54:12 +0200

    Comment out bool_is_set

--- src/binlog_helper.cc
+++ src/binlog_helper.cc
@@ -124,13 +124,13 @@ void set_event_header(drizzle_binlog_event_st* dst,
 }
 
 
-static bool bit_is_set(uint8_t *ptr, int columns, int current_column);
-{
-    if (current_column >= 8)
-    {
-        ptr += current_column / 8;
-        current_column = current_column % 8;
-    }
+// bool bit_is_set(uint8_t *ptr, int columns, int current_column)
+// {
+//     if (current_column >= 8)
+//     {
+//         ptr += current_column / 8;
+//         current_column = current_column % 8;
+//     }
 
-    return ((*ptr) & (1 << current_column));
-}
+//     return ((*ptr) & (1 << current_column));
+// }
