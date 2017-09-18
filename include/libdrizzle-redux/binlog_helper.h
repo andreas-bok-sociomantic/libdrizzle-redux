627c29ea497ce3a9de2b08b20e2cca373649cdf8
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-09-18 18:42:54 +0200

    Add header binlog_helper.h

---
+++ include/libdrizzle-redux/binlog_helper.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief      Check if a binlog event is a rows event
+ *
+ * @param[in]  event_type  a binlog event type
+ *
+ * @return     true if it is a rows event, false otherwise
+ */
+DRIZZLE_API
+bool drizzle_binlog_is_rows_event(drizzle_binlog_event_types_t event_type);
+
+
+#ifdef __cplusplus
+}
+#endif
