8fa2af67bbc84a759039b26c69a4487b3540e67e
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-09-21 18:01:32 +0200

    Add more docs

--- src/rows_event.h
+++ src/rows_event.h
@@ -1,8 +1,95 @@
+/*
+ * Drizzle Client & Protocol Library
+ *
+ * Copyright: Copyright (c) 2017 sociomantic labs GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ *     * The names of its contributors may not be used to endorse or
+ * promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #pragma once
 
 #include <vector>
 
 
+/**
+ * @brief      Rows_event event
+ *
+ * @detailed
+ * Implementation of rows events version 4 which is written by MySQL 5.0 and up
+ *
+ * Three basic kinds of ROWS_EVENT exist:
+ *
+ * @verbatim
+ * +=========================================================================+
+ * | Event             | SQL Cmd | Contents                                  |
+ * +-------------------+---------+-------------------------------------------+
+ * | WRITE_ROWS_EVENT  | INSERT  | the row data to insert                    |
+ * +-------------------+---------+-------------------------------------------+
+ * | DELETE_ROWS_EVENT | DELETE  | as much data as needed to identify a row  |
+ * +-------------------+---------+-------------------------------------------+
+ * | UPDATE_ROWS_EVENT | UPDATE  | as much data as needed to identify a row  |
+ * |                   |         | + the data to change                      |
+ * +=========================================================================+
+ *
+ * Post-header:
+ * if post_header_len == 6 {
+ *     4                  table id
+ * } else {
+ *     6                  table id
+ * }
+ *     2                  flags
+ * if version == 2 {
+ *     2                  extra-data-length
+ *     string.var_len     extra-data
+ * }
+ *
+ * Payload:
+ *     lenenc_int         number of columns
+ *     string.var_len     columns-present-bitmap1, length: (num of columns+7)/8
+ * if UPDATE_ROWS_EVENTv1 or v2 {
+ *     string.var_len     columns-present-bitmap2, length: (num of columns+7)/8
+ * }
+ *
+ * Rows:
+ *     string.var_len     nul-bitmap,
+ *                        length (bits set in 'columns-present-bitmap1'+7)/8
+ *     string.var_len     value of each field as defined in table-map
+ * if UPDATE_ROWS_EVENTv1 or v2 {
+ *     string.var_len     nul-bitmap,
+ *                        length (bits set in 'columns-present-bitmap2'+7)/8
+ *     string.var_len     value of each field as defined in table-map
+ * }
+ *     ... repeat rows until event-end
+ * @endverbatim
+ *
+ * \sa: https://dev.mysql.com/doc/internals/en/rows-event.html
+ */
 struct drizzle_binlog_rows_event_st
 {
     /**
@@ -29,16 +116,49 @@ struct drizzle_binlog_rows_event_st
      * combine result client and column client, field
      */
 
+    /**
+     *  Common header for all binlog events
+     */
     drizzle_binlog_event_st header;
 
+    /**
+     * Id of the associated table
+     */
     uint64_t table_id;
+
+    /**
+     * The table name
+     */
     char table_name[DRIZZLE_MAX_TABLE_SIZE];
+
+    /**
+     * Number of columns in the row
+     */
     uint64_t column_count;
+
+    /**
+     * Length of the metadata block
+     */
     uint32_t field_metadata_len;
+
+    /**
+     * Array defining the type for each column present in the result
+     */
     drizzle_column_type_t *column_type_def;
+
+    /**
+     * Array of type-specific metadata for each column
+     */
     uint8_t *field_metadata;
+
+    /**
+     * Number of bytes required to represent the columns present bitmap
+     */
     uint64_t bitmap_size;
 
+    /**
+     * @brief      Constructor
+     */
     drizzle_binlog_rows_event_st() :
         table_id(0),
         column_count(0),
@@ -48,6 +168,7 @@ struct drizzle_binlog_rows_event_st
         bitmap_size(0)
     {}
 };
+
 
 struct drizzle_binlog_row_events_st
 {
