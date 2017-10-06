c6c8cf0e45d300476f10400a84ed5a427c634fd2
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-10-06 19:41:40 +0200

    Various rows_event impl

--- src/rows_event.cc
+++ src/rows_event.cc
@@ -61,6 +61,15 @@ drizzle_binlog_rows_event_st *drizzle_binlog_parse_rows_event(
     rows_event->bitmap_size = (rows_event->column_count + 7)/8;
 
     // update the mapping between table id and table row
+    uint8_t columns_present[rows_event->column_count];
+    memcpy(&columns_present, event->data_ptr, rows_event->bitmap_size);
+    event->data_ptr += rows_event->bitmap_size;
+
+    while ( drizzle_binlog_event_available_bytes(event) >= DRIZZLE_BINLOG_CRC32_LEN )
+    {
+        drizzle_binlog_parse_row(rows_event, event->data_ptr, columns_present);
+    }
+
     event->binlog_rbr->add_table_row_mapping(rows_event);
     return rows_event;
 }
@@ -82,3 +91,101 @@ const char* drizzle_binlog_rows_event_table_name(
 {
     return event->table_name;
 }
+
+drizzle_return_t drizzle_binlog_parse_row(
+    drizzle_binlog_rows_event_st *event, unsigned char *ptr,
+    unsigned char *columns_present)
+{
+    unsigned metadata_offset = 0;
+    const unsigned char *null_bitmap = ptr;
+    ptr += event->bitmap_size;
+    unsigned idx_null_bitmap = 0;
+
+    for ( unsigned i = 0; i < event->column_count; i++ )
+    {
+        // skip if column is not present
+        if (!bit_is_set(columns_present, i))
+        {
+            continue;
+        }
+
+        auto column_type = event->column_type_def[i];
+
+        // parse the column value
+        if (bit_is_set(null_bitmap, idx_null_bitmap++))
+        {
+            // set null value
+            printf("hello\n");
+
+        }
+        else if (column_protocol_datatype(column_type) == FIXED_STRING)
+        {
+            auto meta_column_type =
+                (drizzle_column_type_t) event->field_metadata[metadata_offset];
+            if (fixed_string_is_enum(meta_column_type))
+            {
+
+                // parse enum
+                // unsigned char val[metadata_offset+1];
+            }
+            else
+            {
+                /**
+                 * PARSE VAR_STRING
+                 * The first byte in the metadata stores the real type of
+                 * the string (ENUM and SET types are also stored as fixed
+                 * length strings).
+                 *
+                 * The first two bits of the second byte contain the XOR'ed
+                 * field length but as that information is not relevant for
+                 * us, we just use this information to know whether to read
+                 * one or two bytes for string length.
+                 */
+
+/*                uint16_t meta = metadata[metadata_offset + 1] + (metadata[metadata_offset] << 8);
+                int bytes = 0;
+                uint16_t extra_length = (((meta >> 4) & 0x300) ^ 0x300);
+                uint16_t field_length = (meta & 0xff) + extra_length;
+
+                if (field_length > 255)
+                {
+                    bytes = ptr[0] + (ptr[1] << 8);
+                    ptr += 2;
+                }
+                else
+                {
+                    bytes = *ptr++;
+                }*/
+            }
+        }
+        else if (column_type == DRIZZLE_COLUMN_TYPE_BIT)
+        {
+
+        }
+        else if (column_type== DRIZZLE_COLUMN_TYPE_NEWDECIMAL)
+        {
+
+        }
+        else if (column_protocol_datatype(column_type) == VARIABLE_STRING)
+        {
+            /*size_t sz;
+            int bytes = metadata[metadata_offset] | metadata[metadata_offset + 1] << 8;*/
+        }
+        else if (column_protocol_datatype(column_type) == BLOB)
+        {
+
+        }
+        else if (column_protocol_datatype(column_type) == TEMPORAL)
+        {}
+        else
+        {
+
+        }
+
+        metadata_offset += get_metadata_len(column_type);
+
+    }
+
+    return DRIZZLE_RETURN_OK;
+
+    }
