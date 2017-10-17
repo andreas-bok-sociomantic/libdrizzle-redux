3c6a19d2c672f04d56d5d1255142a5179acb773b
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-10-17 16:54:47 +0200

    Separtre field getter methods

--- src/rows_event.cc
+++ src/rows_event.cc
@@ -281,22 +281,4 @@ void drizzle_binlog_column_value_st::set_field_value(
     }
 }
 
-drizzle_return_t drizzle_binlog_get_int(drizzle_binlog_row_st *row,
-    size_t field_number, uint32_t *before, uint32_t *after)
-{
-    if (field_number >= row->values_before.size() )
-    {
-        return DRIZZLE_RETURN_INVALID_ARGUMENT;
-    }
 
-    drizzle_binlog_column_value_st *column_value = &row->values_before.at(field_number);
-    *before = column_value->field._uint32;
-
-    if (row->is_update_event)
-    {
-        column_value = &row->values_before.at(field_number);
-        *after = column_value->field._uint32;
-    }
-
-    return DRIZZLE_RETURN_OK;
-}
