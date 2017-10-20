f09679fd12f14aec19b29092600789f1106571d7
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-10-20 19:38:52 +0200

    sdf
    Signed-off-by: Andreas Bok Andersen <andreas.bok@sociomantic.com>

--- src/binlog_row.h
+++ src/binlog_row.h
@@ -3,23 +3,60 @@
 #include <type_traits>
 #include <typeinfo>
 
+/**
+ * @brief      Gets the column value struct at a specified index
+ *
+ *             Checks that all arguments are valid.
+ *
+ * @param      row           The drizzle binlog row struct
+ * @param[in]  field_number  The field number
+ * @param      before        The variable to save before value into
+ * @param      after         The variable to save after value into
+ * @param[in]  ret_ptr       The ret pointer
+ *
+ * @tparam     T             The datatype of the field to get
+ *
+ * @return     Pointer to a column value struct or NULL if arguments are not
+ *             valid
+ */
 template<typename T>
-drizzle_return_t drizzle_binlog_get_field_value(drizzle_binlog_column_value_st *column_value,
+drizzle_binlog_column_value_st *get_column_value_st(drizzle_binlog_row_st *row,
+                                                    size_t field_number,
+                                                    const T *before,
+                                                    const T *after,
+                                                    drizzle_return_t ret_ptr);
+
+template<typename T>
+drizzle_return_t drizzle_binlog_get_field_value(
+    drizzle_binlog_column_value_st *column_value,
     T *dest);
 
-
-/*template<typename T>
-drizzle_return_t drizzle_binlog_get_integer(drizzle_binlog_row_st *row,
-                                        size_t field_number, T *before,
-                                        T *after);*/
-
+template<typename T, typename U>
+drizzle_return_t assign_field_value(drizzle_binlog_row_st *row,
+                                    drizzle_binlog_column_value_st *column_value,
+                                    size_t field_number, T *before, T *after,
+                                    U type1, U type2);
 
 // check if the value is signed/unsigned
 // make a double cast through the actual type and the longest type
 //
-//
-//
-//
 
+/**
+ * @brief      Casts raw bytes to another datatype
+ *             First casts the value to the original datatype specified in the
+ *             table definition, then casts it to the datatype of the output
+ * value
+ *
+ * @param      dest         The variable to assign
+ * @param[in]  src          The raw bytes to cast
+ * @param[in]  is_unsigned  Indicates if the value is signed or unsigned
+ *
+ * @tparam     T_UNSIGNED   The unsigned datatype to cast to
+ * @tparam     T_SIGNED     The signed datatype to cast to
+ * @tparam     TYPE_DEST    The type of the variable to assign
+ */
 template <typename T_UNSIGNED, typename T_SIGNED, typename TYPE_DEST>
-void signedness_cast(TYPE_DEST *dest, const unsigned char *src, bool is_unsigned);
+void signedness_cast(TYPE_DEST *dest, const unsigned char *src,
+                     bool is_unsigned);
+
+
