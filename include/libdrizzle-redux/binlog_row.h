b24f7dcaecad08c34fe030169b645d945ec5fb8d
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-10-23 13:11:48 +0200

    add int bigint

--- include/libdrizzle-redux/binlog_row.h
+++ include/libdrizzle-redux/binlog_row.h
@@ -13,21 +13,21 @@ extern "C" {
  *
  * @return     return value
  */
-/*DRIZZLE_API
+DRIZZLE_API
 drizzle_return_t drizzle_binlog_get_int(drizzle_binlog_row_st *row,
                                         size_t field_number, uint32_t *before,
                                         uint32_t *after);
-*/
+
 DRIZZLE_API
 drizzle_return_t drizzle_binlog_get_uint(drizzle_binlog_row_st *row,
                                         size_t field_number, uint32_t *before,
                                         uint32_t *after);
 
-/*DRIZZLE_API
+DRIZZLE_API
 drizzle_return_t drizzle_binlog_get_big_int(drizzle_binlog_row_st *row,
                                            size_t field_number,
                                            uint64_t *before,
-                                           uint64_t *after);*/
+                                           uint64_t *after);
 
 DRIZZLE_API
 drizzle_return_t drizzle_binlog_get_big_uint(drizzle_binlog_row_st *row,
