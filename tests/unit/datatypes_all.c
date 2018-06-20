c75954393e724486b095a1a9d63e2d277d299409
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2018-06-20 17:41:48 +0200

    datatypes all

--- tests/unit/datatypes_all.c
+++ tests/unit/datatypes_all.c
@@ -47,6 +47,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wswitch-enum"
+
 #define CHECKED_QUERY(cmd) \
   result = drizzle_query(con, cmd, 0, &driz_ret); \
   ASSERT_EQ_(driz_ret, DRIZZLE_RETURN_OK, "Error (%s): %s, from \"%s\"", \
@@ -57,19 +60,126 @@
   ASSERT_EQ_(driz_ret, DRIZZLE_RETURN_OK, "Error (%s): %s, in \"%s\"", \
              drizzle_strerror(driz_ret), drizzle_error(con), #s);
 
-/*static const drizzle_column_type_t expected_column_types[10] = {
-    DRIZZLE_COLUMN_TYPE_NONE, // Columns are 1-indexed
-    DRIZZLE_COLUMN_TYPE_LONG,
-    DRIZZLE_COLUMN_TYPE_DATE, DRIZZLE_COLUMN_TYPE_YEAR,
-    DRIZZLE_COLUMN_TYPE_TIMESTAMP, DRIZZLE_COLUMN_TYPE_TIMESTAMP,
-    DRIZZLE_COLUMN_TYPE_TIME, DRIZZLE_COLUMN_TYPE_TIME,
-    DRIZZLE_COLUMN_TYPE_DATETIME,
-    //  DRIZZLE_COLUMN_TYPE_NEWDATE,
-    DRIZZLE_COLUMN_TYPE_DATETIME,
-};*/
+struct test_column_st
+{
+  const char name[1024];
+  const char value[128];
+  const drizzle_column_type_t type;
+};
 
-/*static const char *column_names[10] = {NULL, "a", "b", "c", "d",
-                                       "e",  "f", "g", "h", "i"};*/
+typedef struct test_column_st test_column_st;
+test_column_st test_columns[] = {
+  {"varchar", "varchar", DRIZZLE_COLUMN_TYPE_VAR_STRING},
+  {"tinyint", "0", DRIZZLE_COLUMN_TYPE_TINY},
+  {"text", "text", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"date", "2017-04-28", DRIZZLE_COLUMN_TYPE_DATE},
+  {"smallint", "32687", DRIZZLE_COLUMN_TYPE_SHORT},
+  {"mediumint", "8388351", DRIZZLE_COLUMN_TYPE_INT24},
+  {"int", "2147352575", DRIZZLE_COLUMN_TYPE_LONG},
+  {"bigint", "9222246136947920895", DRIZZLE_COLUMN_TYPE_LONGLONG},
+  {"float", "1.23", DRIZZLE_COLUMN_TYPE_FLOAT},
+  {"double", "2.3456", DRIZZLE_COLUMN_TYPE_DOUBLE},
+  {"decimal", "123.46", DRIZZLE_COLUMN_TYPE_NEWDECIMAL},
+  {"datetime", "2017-04-28 13:54:05", DRIZZLE_COLUMN_TYPE_DATETIME},
+  {"timestamp", "2018-03-12 00:00:00", DRIZZLE_COLUMN_TYPE_TIMESTAMP},
+  {"time", "13:54:05", DRIZZLE_COLUMN_TYPE_TIME},
+  {"year", "2017", DRIZZLE_COLUMN_TYPE_YEAR},
+  {"char", "char", DRIZZLE_COLUMN_TYPE_STRING},
+  {"tinyblob", "1100", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"tinytext", "tinytext", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"blob", "11001100", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"mediumblob", "110011001100", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"mediumtext", "mediumtext", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"longblob", "1100110011001100", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"longtext", "longtext", DRIZZLE_COLUMN_TYPE_BLOB},
+  {"enum", "1", DRIZZLE_COLUMN_TYPE_STRING},
+  {"set", "2,3", DRIZZLE_COLUMN_TYPE_STRING},
+  {"bool", "1", DRIZZLE_COLUMN_TYPE_TINY},
+  {"binary", "1100110011001100", DRIZZLE_COLUMN_TYPE_STRING},
+  {"varbinary", "1100110011001100", DRIZZLE_COLUMN_TYPE_VAR_STRING}
+};
+
+void check_column_value(char *actual, test_column_st expected, drizzle_column_st *column_st, drizzle_stmt_st *stmt, int col_idx);
+void check_column_value(char *actual, test_column_st expected, drizzle_column_st *column_st, drizzle_stmt_st *stmt, int col_idx)
+{
+
+  ASSERT_STREQ_(actual, expected.value,
+        "Retrieved bad row value %s - expected %s", actual,
+        expected.value);
+  ASSERT_EQ_(drizzle_column_type(column_st), expected.type,
+    "Column type 'DRIZZLE_COLUMN_TYPE_%s' resolved to wrong name: '%s'",
+    drizzle_column_type_str(drizzle_column_type(column_st)),
+    drizzle_column_type_str(expected.type));
+}
+
+#define CHECK_INSERT_QUERY(actual, expected, column_st) \
+  check_column_value(actual, expected, column_st, NULL, -1);
+
+void CHECK_PARAM(drizzle_stmt_st *stmt, int col_idx);
+void CHECK_PARAM(drizzle_stmt_st *stmt, int col_idx)
+{
+
+  size_t len;
+  drizzle_return_t driz_ret;
+  const char *col_strval;
+  test_column_st expected = test_columns[col_idx];
+      switch(expected.type)
+      {
+        case DRIZZLE_COLUMN_TYPE_NULL:
+          /* I don't think this one should happen, but cover it anyway */
+          break;
+        case DRIZZLE_COLUMN_TYPE_TINY:
+        case DRIZZLE_COLUMN_TYPE_SHORT:
+        case DRIZZLE_COLUMN_TYPE_YEAR:
+        case DRIZZLE_COLUMN_TYPE_INT24:
+        case DRIZZLE_COLUMN_TYPE_LONG:
+          //drizzle_stmt_get_int()
+         // drizzle_stmt_get_int_from_name
+          break;
+        case DRIZZLE_COLUMN_TYPE_LONGLONG:
+          // drizzle_stmt_get_bigint_from_name
+          // drizzle_stmt_get_bigint
+          break;
+        case DRIZZLE_COLUMN_TYPE_FLOAT:
+
+          break;
+        case DRIZZLE_COLUMN_TYPE_DOUBLE:
+          //drizzle_stmt_get_double
+          // drizzle_stmt_get_double_from_name
+          break;
+        case DRIZZLE_COLUMN_TYPE_TIME:
+        case DRIZZLE_COLUMN_TYPE_DATE:
+        case DRIZZLE_COLUMN_TYPE_DATETIME:
+        case DRIZZLE_COLUMN_TYPE_TIMESTAMP:
+        case DRIZZLE_COLUMN_TYPE_TINY_BLOB:
+        case DRIZZLE_COLUMN_TYPE_MEDIUM_BLOB:
+        case DRIZZLE_COLUMN_TYPE_LONG_BLOB:
+        case DRIZZLE_COLUMN_TYPE_BLOB:
+        case DRIZZLE_COLUMN_TYPE_BIT:
+        case DRIZZLE_COLUMN_TYPE_STRING:
+        case DRIZZLE_COLUMN_TYPE_VAR_STRING:
+        case DRIZZLE_COLUMN_TYPE_DECIMAL:
+        case DRIZZLE_COLUMN_TYPE_NEWDECIMAL:
+        case DRIZZLE_COLUMN_TYPE_NEWDATE:
+        case DRIZZLE_COLUMN_TYPE_VARCHAR:
+        case DRIZZLE_COLUMN_TYPE_ENUM:
+        case DRIZZLE_COLUMN_TYPE_SET:
+        case DRIZZLE_COLUMN_TYPE_GEOMETRY:
+        case DRIZZLE_COLUMN_TYPE_TIMESTAMP2:
+        case DRIZZLE_COLUMN_TYPE_DATETIME2:
+        case DRIZZLE_COLUMN_TYPE_TIME2:
+          col_strval = drizzle_stmt_get_string(stmt, col_idx, &len, &driz_ret);
+          ASSERT_STREQ_(col_strval, expected.value,
+          "Retrieved bad column[%d] %s value %s - expected %s", col_idx, drizzle_column_type_str(expected.type), col_strval,
+          expected.value);
+          break;
+        default:
+          printf("ERROR\n");
+          break;
+      }
+}
+#define CHECK_INSERT_STMT(actual, expected, column_st, stmt, col_idx) \
+  check_column_value(actual, expected, column_st, stmt, col_idx);
 
 int main(int argc, char *argv[])
 {
@@ -78,11 +188,9 @@ int main(int argc, char *argv[])
   drizzle_result_st *result;
   drizzle_return_t driz_ret;
   drizzle_row_t row;
-  int num_fields;
   const char *query;
   drizzle_column_st *column;
   drizzle_stmt_st *sth;
-  unsigned rows_in_table;
 
   set_up_connection();
   set_up_schema("test_datatypes");
@@ -107,15 +215,13 @@ int main(int argc, char *argv[])
                 "`time` TIME NOT NULL ,"
                 "`year` YEAR NOT NULL ,"
                 "`char` CHAR( 10 ) NOT NULL ,"
-                "`tinyblob` TINYBLOB NOT NULL ,"
+                "`tinyblob` TINYBLOB NOT NULL,"
                 "`tinytext` TINYTEXT NOT NULL ,"
                 "`blob` BLOB NOT NULL ,"
-                "`mediumblob` MEDIUMBLOB NOT NULL ,"
+                "`mediumblob` MEDIUMBLOB NOT NULL,"
                 "`mediumtext` MEDIUMTEXT NOT NULL ,"
                 "`longblob` LONGBLOB NOT NULL ,"
                 "`longtext` LONGTEXT NOT NULL ,"
-                /*"`enum` ENUM( '1', '2', '3' ) NOT NULL ,"
-                "`set` SET( '1', '2', '3' ) NOT NULL ,"*/
                 "`enum` ENUM( '1', '2', '3' ) ,"
                 "`set` SET( '1', '2', '3' ) ,"
                 "`bool` BOOL NOT NULL ,"
@@ -138,15 +244,15 @@ int main(int argc, char *argv[])
                 "0,"
                 "'text',"
                 "'2017-04-28',"
-                "1,"
-                "2,"
-                "4,"
-                "5,"
+                "32687,"
+                "8388351,"
+                "2147352575,"
+                "9222246136947920895,"
                 "1.23,"
                 "2.3456,"
                 "123.4567,"
                 "'2017-04-28 13:54:05',"
-                "now(),"
+                "TIMESTAMP('2018-03-12'),"
                 "'13:54:05',"
                 "2017,"
                 "'char',"
@@ -157,23 +263,64 @@ int main(int argc, char *argv[])
                 "'mediumtext',"
                 "'1100110011001100',"
                 "'longtext',"
-                "('1'),"
-                "('4'),"
+                "'1',"
+                "'2,3',"
                 "1,"
                 "'1100110011001100',"
                 "'1100110011001100'"
                 ");");
   ASSERT_EQ(drizzle_result_affected_rows(result), 1);
 
+  CHECKED_QUERY("SELECT * FROM t1");
+
+  drizzle_result_buffer(result);
+
+  ASSERT_EQ_(drizzle_result_column_count(result), 28,
+    "Invalid number of columns in resultset");
+
+  while ((row = drizzle_row_next(result)))
+  {
+    int col_idx = 0;
+    while ((column= drizzle_column_next(result)))
+    {
+      CHECK_INSERT_QUERY(row[col_idx], test_columns[col_idx], column);
+      col_idx++;
+    }
+  }
+  drizzle_result_free(result);
+
+  //CHECKED_QUERY("DELETE FROM test_datatypes.t1");
+
   query = "INSERT INTO t1 ("
-          "`varchar`, `tinyint`, `text`, `date`,"
-          "`smallint`, `mediumint`, `int`, `bigint`,"
-          "`float`, `double`, `decimal`, `datetime`,"
-          "`timestamp`, `time`, `year`, `char`,"
-          "`tinyblob`, `tinytext`, `blob`, `mediumblob`,"
-          "`mediumtext`, `longblob`, `longtext`,"
-          "`enum`, `set`, `bool`, `binary`, `varbinary`"
-          ")"
+          "`varchar`,"
+          "`tinyint`,"
+          "`text`,"
+          "`date`,"
+          "`smallint`,"
+          "`mediumint`,"
+          "`int`,"
+          "`bigint`,"
+          "`float`,"
+          "`double`,"
+          "`decimal`,"
+          "`datetime`,"
+          "`timestamp`,"
+          "`time`,"
+          "`year`,"
+          "`char`,"
+          "`tinyblob`,"
+          "`tinytext`,"
+          "`blob`,"
+          "`mediumblob`,"
+          "`mediumtext`,"
+          "`longblob`,"
+          "`longtext`,"
+          "`enum`,"
+          "`set`,"
+          "`bool`,"
+          "`binary`,"
+          "`varbinary`"
+          ") "
           "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
   sth = drizzle_stmt_prepare(con, query, strlen(query), &driz_ret);
@@ -183,58 +330,115 @@ int main(int argc, char *argv[])
 
   // Set all statement parameters;
   CHECK(drizzle_stmt_set_string(sth, 0, "varchar", strlen("varchar")));
-  CHECK(drizzle_stmt_set_tiny(sth, 1, 1, 0));
+  CHECK(drizzle_stmt_set_tiny(sth, 1, 0, 0));
   CHECK(drizzle_stmt_set_string(sth, 2, "text", strlen("text")));
-  CHECK(drizzle_stmt_set_timestamp(sth, 3, 2018, 1, 1, 0, 0, 0, 0));
+  CHECK(drizzle_stmt_set_timestamp(sth, 3, 2017, 4, 28, 0, 0, 0, 0));
   CHECK(drizzle_stmt_set_short(sth, 4, 32687, 0));
   CHECK(drizzle_stmt_set_int(sth, 5, 8388351, 0));
   CHECK(drizzle_stmt_set_int(sth, 6, 2147352575, 0));
   CHECK(drizzle_stmt_set_bigint(sth, 7, 9222246136947920895, 0));
-  CHECK(drizzle_stmt_set_float(sth, 8, 443664.0f));
-  CHECK(drizzle_stmt_set_double(sth, 9, 291.2711110711098l));
-  CHECK(drizzle_stmt_set_double(sth, 10, 123.4567));
-  CHECK(drizzle_stmt_set_timestamp(sth, 11, 2001, 9, 11, 10, 3, 11, 0));
-  CHECK(drizzle_stmt_set_timestamp(sth, 12, 2002, 10, 11, 10, 3, 11, 100));
-  CHECK(drizzle_stmt_set_time(sth, 13, 3, 6, 15, 3, 0, 0));
-  CHECK(drizzle_stmt_set_int(sth, 14, 2002, 0));
+  CHECK(drizzle_stmt_set_float(sth, 8, 1.23f));
+  CHECK(drizzle_stmt_set_double(sth, 9, 2.3456l));
+  CHECK(drizzle_stmt_set_double(sth, 10, 123.4567l));
+  CHECK(drizzle_stmt_set_timestamp(sth, 11, 2017, 4, 28, 13, 54, 05, 0));
+  CHECK(drizzle_stmt_set_timestamp(sth, 12, 2018, 03, 12, 0, 0, 0, 0));
+  CHECK(drizzle_stmt_set_time(sth, 13, 0, 13, 54, 5, 0, 0));
+  CHECK(drizzle_stmt_set_int(sth, 14, 2017, 0));
   CHECK(drizzle_stmt_set_string(sth, 15, "char", strlen("char")));
-  CHECK(drizzle_stmt_set_string(sth, 16, "1100", strlen("1100")));
+  CHECK(drizzle_stmt_set_string(sth, 16, "1100", 4));
   CHECK(drizzle_stmt_set_string(sth, 17, "tinytext", strlen("tinytext")));
-  CHECK(drizzle_stmt_set_string(sth, 18, "1100110011001100",
-    strlen("1100110011001100")));
-  CHECK(drizzle_stmt_set_string(sth, 19, "11001100", strlen("11001100")));
+  CHECK(drizzle_stmt_set_string(sth, 18, "11001100", strlen("11001100")));
+  CHECK(drizzle_stmt_set_string(sth, 19, "110011001100", strlen("110011001100")));
   CHECK(drizzle_stmt_set_string(sth, 20, "mediumtext", strlen("mediumtext")));
-  CHECK(drizzle_stmt_set_string(sth, 21, "11001100110011001100110011001100",
-    strlen("11001100110011001100110011001100")));
+  CHECK(drizzle_stmt_set_string(sth, 21, "1100110011001100", strlen("1100110011001100")));
+  /*CHECK(drizzle_stmt_set_string(sth, 21, "0x11001100110011001100110011001100",
+    strlen("11001100110011001100110011001100")));*/
   CHECK(drizzle_stmt_set_string(sth, 22, "longtext", strlen("longtext")));
-  /*CHECK(drizzle_stmt_set_string(sth, 23, "('1')", strlen("('1')")));
-  CHECK(drizzle_stmt_set_string(sth, 24, "('4')", strlen("('4')")));*/
-  CHECK(drizzle_stmt_set_null(sth, 23));
-  CHECK(drizzle_stmt_set_null(sth, 24));
+  CHECK(drizzle_stmt_set_string(sth, 23, "1", 1));
+  CHECK(drizzle_stmt_set_string(sth, 24, "2,3", 3));
   CHECK(drizzle_stmt_set_int(sth, 25, 1, 0));
-  CHECK(drizzle_stmt_set_string(sth, 26, "1100110011001100",
-    strlen("1100110011001100")));
-  CHECK(drizzle_stmt_set_string(sth, 27, "1100110011001100",
-    strlen("1100110011001100")));
+  /*char value_buffer[128];
+  drizzle_hex_string(value_buffer, (const unsigned char *) "binary", strlen("binary"));
+  CHECK(drizzle_stmt_set_string(sth, 26,  value_buffer, strlen(value_buffer)));*/
+  CHECK(drizzle_stmt_set_string(sth, 26,  "1100110011001100", strlen("1100110011001100")));
+  CHECK(drizzle_stmt_set_string(sth, 27, "1100110011001100", strlen("1100110011001100")));
 
   driz_ret = drizzle_stmt_execute(sth);
   ASSERT_EQ_(driz_ret, DRIZZLE_RETURN_OK, "Error (%s): %s, executing \"%s\"",
              drizzle_strerror(driz_ret), drizzle_error(con), query);
+  driz_ret = drizzle_stmt_buffer(sth);
+  ASSERT_EQ_(driz_ret, DRIZZLE_RETURN_OK, "Error (%s): %s, buffering \"%s\"",
+             drizzle_strerror(driz_ret), drizzle_error(con), query);
 
+  ASSERT_EQ(drizzle_stmt_affected_rows(sth), 1);
 
-  CHECKED_QUERY("SELECT * FROM t1");
+  ASSERT_EQ_(DRIZZLE_RETURN_OK, driz_ret, "%s", drizzle_error(con));
 
-  drizzle_result_buffer(result);
+  CHECK(drizzle_stmt_close(sth));
 
-  while ((row = drizzle_row_next(result)))
-  {
-    while ((column= drizzle_column_next(result)))
+  query = "SELECT "
+          "`varchar`,"
+          "`tinyint`,"
+          "`text`,"
+          "`date`,"
+          "`smallint`,"
+          "`mediumint`,"
+          "`int`,"
+          "`bigint`,"
+          "`float`,"
+          "`double`,"
+          "`decimal`,"
+          "`datetime`,"
+          "`timestamp`,"
+          "`time`,"
+          "`year`,"
+          "`char`,"
+          "`tinyblob`,"
+          "`tinytext`,"
+          "`blob`,"
+          "`mediumblob`,"
+          "`mediumtext`,"
+          "`longblob`,"
+          "`longtext`,"
+          "`enum`,"
+          "`set`,"
+          "`bool`,"
+          "`binary`,"
+          "`varbinary`"
+          "FROM test_datatypes.t1";
+
+  sth = drizzle_stmt_prepare(con, query, strlen(query), &driz_ret);
+
+  ASSERT_EQ_(driz_ret, DRIZZLE_RETURN_OK, "Error (%s): %s, preparing \"%s\"",
+  drizzle_strerror(driz_ret), drizzle_error(con), query);
+
+  driz_ret = drizzle_stmt_execute(sth);
+  ASSERT_EQ_(driz_ret, DRIZZLE_RETURN_OK, "Error (%s): %s, executing \"%s\"",
+             drizzle_strerror(driz_ret), drizzle_error(con), query);
+  driz_ret = drizzle_stmt_buffer(sth);
+  ASSERT_EQ_(driz_ret, DRIZZLE_RETURN_OK, "Error (%s): %s, buffering \"%s\"",
+             drizzle_strerror(driz_ret), drizzle_error(con), query);
+  int column_count= drizzle_stmt_column_count(sth);
+  ASSERT_EQ(column_count, 28);
+  size_t len;
+  while (drizzle_stmt_fetch(sth) != DRIZZLE_RETURN_ROW_END) {
+    int col_idx = 0;
+    const char *col_strval;
+
+    while (col_idx < column_count)
     {
-
+      /*col_strval = drizzle_stmt_get_string(sth, col_idx, &len, &driz_ret);
+      printf("Retrieved column value %s - expected %s\n", col_strval,
+        test_columns[col_idx].value);*/
+      /*ASSERT_STREQ_(col_strval, test_columns[col_idx].value,
+        "Retrieved bad row value %s - expected %s", col_strval,
+        test_columns[col_idx].value);*/
+      CHECK_PARAM(sth, col_idx);
+      col_idx++;
     }
   }
-  drizzle_result_free(result);
 
+  CHECK(drizzle_stmt_close(sth));
 
   CHECKED_QUERY("DROP TABLE test_datatypes.t1");
 
@@ -242,3 +446,4 @@ int main(int argc, char *argv[])
 
   return EXIT_SUCCESS;
 }
+#pragma GCC diagnostic pop
